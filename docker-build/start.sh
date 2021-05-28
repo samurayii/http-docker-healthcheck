9b0911050f2ee4d7c94ee7aef6836dd13df8f6b1
Alexandr <samurayiii1986@gmail.com>
2021-05-28 16:54:06 +0300

    0.2.6

--- docker-build/start.sh
+++ docker-build/start.sh
@@ -3,6 +3,6 @@
 if [[ $APP_RESTARTER ]]; then
     app-restarter -e "node app.js --config ./config.toml" -tmp /app_restarter_tmp -c /http-docker-healthcheck
 else
-    cd /${APP_NAME}
+    cd /http-docker-healthcheck
     node app.js --config ./config.toml
 fi
